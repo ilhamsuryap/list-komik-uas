af02fc4abe83fb51a784dbde7d762f9b30962636
ilhamsuryap <ilhamsurya123123@gmail.com>
2024-06-01 18:01:05 +0700

    merubah tampilan home

--- app/src/main/java/com/example/uas_listkomik/home/HomeFragment.kt
+++ app/src/main/java/com/example/uas_listkomik/home/HomeFragment.kt
@@ -31,14 +31,11 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupRecyclerView()
 
-        binding.manhwa.setOnClickListener {
-            filterList(CharCategory.MANHWA) // Panggil filterList dengan kategori MANHWA
-        }
-        binding.manga.setOnClickListener {
-            filterList(CharCategory.MANGA) // Panggil filterList dengan kategori MANGA
-        }
+        binding.rvDataList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        val data = generateDataList()
+        binding.rvDataList.adapter = KomikAdapter(data)
     }
 
     override fun onDestroyView() {
@@ -46,21 +43,7 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    private fun setupRecyclerView() {
-        val context = activity ?: return
-        binding.rvDataList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        val data = generateDataList()
-        binding.rvDataList.adapter = KomikAdapter(data)
-    }
 
-    private fun filterList(category: CharCategory) {
-        val data = generateDataList()
-        val filteredList = data.filter {
-            it.kategori == category
-        }
-        binding.rvDataList.adapter = KomikAdapter(filteredList)
-        if (filteredList.isEmpty()) {
-            Toast.makeText(requireContext(), "Data tidak ditemukan", Toast.LENGTH_SHORT).show()
-        }
-    }
+
+
 }
